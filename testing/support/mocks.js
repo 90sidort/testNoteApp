e1ee64910320a9102d9fb2a44302a83e26c3582f
unknown <dabroszynwielki@gmail.com>
2020-03-11 22:37:19 +0100

    mocks updated

--- testing/support/mocks.js
+++ testing/support/mocks.js
@@ -3,6 +3,7 @@ export const singleWorkMock = [
         "id": "e2e6bb4f-f870-4dd0-a93f-84eb73abd93c",
         "title": "Test1",
         "body": "Test1_1",
+        "priority": false,
         "createdAt": 1583668335621,
         "updatedAt": 1583668343975,
         "category": "work"
@@ -13,6 +14,7 @@ export const singleHomeMock = [
         "id": "e2e6bb4f-f870-4dd0-a93f-84eb73abd93c",
         "title": "Test1",
         "body": "Test1_1",
+        "priority": false,
         "createdAt": 1583668335621,
         "updatedAt": 1583668343975,
         "category": "home"
@@ -23,6 +25,7 @@ export const singleOtherMock = [
         "id": "e2e6bb4f-f870-4dd0-a93f-84eb73abd93c",
         "title": "Test1",
         "body": "Test1_1",
+        "priority": false,
         "createdAt": 1583668335621,
         "updatedAt": 1583668343975,
         "category": "other"
@@ -33,6 +36,7 @@ export const singleAllMock = [
         "id": "35edb01b-89e1-4816-b65d-4c5fadbbe8c1",
         "title": "Test2",
         "body": "Test2_2",
+        "priority": false,
         "createdAt": 1583682529312,
         "updatedAt": 1583682536023,
         "category": "home"
@@ -41,6 +45,7 @@ export const singleAllMock = [
         "id": "80fa90d2-b024-49ea-869b-f5ea2c18e0f8",
         "title": "Test1",
         "body": "Test1_1",
+        "priority": false,
         "createdAt": 1583682520764,
         "updatedAt": 1583682526404,
         "category": "work"
@@ -49,6 +54,7 @@ export const singleAllMock = [
         "id": "4d14a122-3cae-42e2-8b55-7b824a80e202",
         "title": "Test3",
         "body": "Test3_3",
+        "priority": false,
         "createdAt": 1583682538992,
         "updatedAt": 1583682551467,
         "category": "other"
@@ -59,6 +65,7 @@ export const filteringSetMock = [
         "id": "7d739516-1621-4a2b-b75b-c5e4a9530fd5",
         "title": "TestZOther",
         "body": "",
+        "priority": false,
         "createdAt": 1583847832165,
         "updatedAt": 1583847839956,
         "category": "other"
@@ -67,6 +74,7 @@ export const filteringSetMock = [
         "id": "d4ad7ab9-abe5-4208-b3e9-e39b5d864980",
         "title": "TestYOther",
         "body": "",
+        "priority": false,
         "createdAt": 1583847814985,
         "updatedAt": 1583847823495,
         "category": "other"
@@ -75,6 +83,7 @@ export const filteringSetMock = [
         "id": "cc30059f-c62e-49bc-a0e2-bca2bfa9b576",
         "title": "TestDHome",
         "body": "",
+        "priority": false,
         "createdAt": 1583847787229,
         "updatedAt": 1583847801551,
         "category": "home"
@@ -83,6 +92,7 @@ export const filteringSetMock = [
         "id": "bc7bf789-50dc-4003-a118-51b245c70240",
         "title": "TestFHome",
         "body": "",
+        "priority": false,
         "createdAt": 1583847763860,
         "updatedAt": 1583847782957,
         "category": "home"
@@ -91,6 +101,7 @@ export const filteringSetMock = [
         "id": "28108da5-5103-4ec0-96fd-0b4933423c9a",
         "title": "TestEHome",
         "body": "",
+        "priority": false,
         "createdAt": 1583847750688,
         "updatedAt": 1583847759705,
         "category": "home"
@@ -99,6 +110,7 @@ export const filteringSetMock = [
         "id": "433ef9a9-fee1-4515-bdea-7bc1c163d181",
         "title": "TestBWork",
         "body": "",
+        "priority": false,
         "createdAt": 1583847714089,
         "updatedAt": 1583847717504,
         "category": "work"
@@ -107,6 +119,7 @@ export const filteringSetMock = [
         "id": "0bf259e4-fb13-434c-99ac-96e700f98287",
         "title": "TestCWork",
         "body": "",
+        "priority": false,
         "createdAt": 1583847701423,
         "updatedAt": 1583847706932,
         "category": "work"
@@ -115,6 +128,7 @@ export const filteringSetMock = [
         "id": "d42de6d2-04fd-4acf-bec2-800873520909",
         "title": "TestAWork",
         "body": "",
+        "priority": false,
         "createdAt": 1583847688609,
         "updatedAt": 1583847697663,
         "category": "work"
@@ -123,6 +137,7 @@ export const filteringSetMock = [
         "id": "ba9a2b03-2628-4efc-9b18-b18220dc3e05",
         "title": "TestXOther",
         "body": "",
+        "priority": false,
         "createdAt": 1583847846439,
         "updatedAt": 1583847856346,
         "category": "other"
